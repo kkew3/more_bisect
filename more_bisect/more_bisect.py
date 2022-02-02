0e8b0f53cb7d987d54ff85e02026ca725c286ea6
kkew3 <kps6326@hotmail.com>
2022-02-02 18:25:27 +0800

    Populated __all__.

--- more_bisect/more_bisect.py
+++ more_bisect/more_bisect.py
@@ -29,6 +29,12 @@ __all__ = [
     'any_pos_eq',
     'first_pos_eq',
     'last_pos_eq',
+    'last_pos_lt',
+    'first_pos_gt',
+    'bisect_left',
+    'bisect_right',
+    'last_closest_to',
+    'first_closest_to',
 ]
 
 
